e6873432850f3e7c3088d33f70d547fa206d3351
Sanfra1407 <1427939+Sanfra1407@users.noreply.github.com>
2019-02-04 22:49:45 +0100

    Webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          output: null,
+          keep_fnames: false,
+        },
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)?$/,
+        exclude: /(node_modules)/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  }
+}
