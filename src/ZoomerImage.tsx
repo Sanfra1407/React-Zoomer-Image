260ececc01461815a77e70a536d63de2d97f44e6
Giuseppe Sanfrancesco <sanfra1407@live.it>
2024-02-06 23:48:15 +0100

    Update Component
    Updated component. Removed the old class approach in favour of the new one with hooks

--- src/ZoomerImage.tsx
+++ src/ZoomerImage.tsx
@@ -1,22 +1,14 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import "./zoomer.scss";
 
-/**
- * @interface IProps
- * @description Component's props
- */
-interface IProps {
+type PropsType = {
   zoomId: string;
   imgSrc: string;
   imgAlt?: string;
   imgTitle?: string;
 }
 
-/**
- * @interface IState
- * @description Component's state
- */
-interface IState {
+type StateType = {
   scale: number;
   offsetX: number;
   offsetY: number;
@@ -24,23 +16,44 @@ interface IState {
   transitioning: boolean;
 }
 
-class ZoomerImage extends Component<IProps, IState> {
-  state = {
-    zoomed: false,
-    transitioning: false,
+/**
+ * @property {HTMLElement} overlay
+ * @description HTML Element to add the overlay to the page
+ */
+let overlay: HTMLElement = null;
+
+const TIMEOUT_TIMER = 280;
+
+function ZoomerImage({ zoomId, imgAlt, imgTitle, imgSrc }: PropsType) {
+  const initialState: StateType = {
+    scale: 1,
     offsetX: 0,
     offsetY: 0,
-    scale: 1
+    zoomed: false,
+    transitioning: false,
   };
 
+  const [state, setState] = useState<StateType>(initialState);
+
+  useEffect(() => {
+    if (!document.querySelector(".zoomer__backdrop--hidden")) {
+      overlay = document.createElement("div");
+      overlay.className = "zoomer__backdrop--hidden";
+      document.body.appendChild(overlay);
+    } else {
+      overlay = document.querySelector(".zoomer__backdrop--hidden");
+    }
+  }, []);
+
+  useEffect(() => {
+    overlay.className = `${state.zoomed ? "zoomer__backdrop" : "zoomer__backdrop--hidden"}`;
+  });
+
   /**
-   * @method _getOffsets
+   * @function getOffsets
    * @description Function for getting the image coordinates for the animation
-   *
-   * @returns {any}
    */
-  _getOffsets = (): any => {
-    const { zoomId } = this.props;
+  const getOffsets = (): any => {
     const halfScreenX: number = window.innerWidth / 2;
     const halfScreenY: number = window.innerHeight / 2;
     const $el: HTMLElement = document.querySelector(
@@ -53,8 +66,7 @@ class ZoomerImage extends Component<IProps, IState> {
 
     const heightRatio: number = window.innerHeight / $el.clientHeight;
     const widthRatio: number = window.innerWidth / $el.clientWidth;
-    let scale: number = heightRatio > widthRatio ? widthRatio : heightRatio;
-    scale *= 0.8;
+    const scale: number = (heightRatio > widthRatio ? widthRatio : heightRatio) * 0.8;
 
     return {
       offsetX,
@@ -64,132 +76,65 @@ class ZoomerImage extends Component<IProps, IState> {
   };
 
   /**
-   * @method zoom
+   * @function zoom
    * @description Function for the zoom effect
-   *
-   * @returns {void}
    */
-  zoom = (): void => {
-    const { offsetX, offsetY, scale } = this._getOffsets();
+  const zoom = (): void => {
+    const { offsetX, offsetY, scale } = getOffsets();
 
-    this.setState(
-      prevState => {
-        return {
-          zoomed: !prevState.zoomed,
-          offsetX: !prevState.zoomed ? offsetX : 0,
-          offsetY: !prevState.zoomed ? offsetY : 0
-        };
-      },
-      () => {
-        this.setState(
-          {
-            transitioning: true,
-            scale: this.state.zoomed ? scale : 1
-          },
-          () => {
-            setTimeout(() => {
-              this.setState({
-                transitioning: false
-              });
-            }, 290);
+    setState(prevState => {
+      return {
+        scale: !prevState.zoomed ? scale : 1,
+        zoomed: !prevState.zoomed,
+        offsetX: !prevState.zoomed ? offsetX : 0,
+        transitioning: true,
+        offsetY: !prevState.zoomed ? offsetY : 0
+      };
+    });
+
+    if (state.zoomed) {
+      setTimeout(() => {
+        setState((prevState) => {
+          return {
+            ...prevState,
+            zoomed: false,
           }
-        );
-      }
-    );
+        });
+      }, TIMEOUT_TIMER);
+    }
 
-    document
-      .querySelector("body")
-      .removeEventListener("keydown", this._keyPressListener);
-    document.querySelector("body").removeEventListener("wheel", this._zoomOut);
-    window.removeEventListener("resize", this._zoomOut);
+    setTimeout(() => {
+      setState((prevState) => {
+        return {
+          ...prevState,
+          transitioning: false,
+        }
+      });
+    }, TIMEOUT_TIMER);
   };
 
-  /**
-   * @method _zoomOut
-   * @description Function for removing the zoom
-   *
-   * @returns {void}
-   */
-  _zoomOut = (): void => {
-    if (this.state.zoomed && !this.state.transitioning) {
-      this.zoom();
-    }
-  };
-
-  /**
-   * @method _keyPressListener
-   * @description Function to be called when the ESC button is pressed
-   *
-   * @returns {void}
-   */
-  _keyPressListener = (e: any): void => {
-    if (e.keyCode === 27) {
-      this._zoomOut();
-    }
-  };
-
-  /**
-   * @property {HTMLElement} overlay
-   * @description HTML Element to add the overlay to the page
-   */
-  overlay: HTMLElement = null;
-
-  constructor(props) {
-    super(props);
-
-    if (!document.querySelector(".zoomer__backdrop--hidden")) {
-      this.overlay = document.createElement("div");
-      this.overlay.className = "zoomer__backdrop--hidden";
-      document.body.appendChild(this.overlay);
-    } else {
-      this.overlay = document.querySelector(".zoomer__backdrop--hidden");
-    }
-  }
-
-  componentDidUpdate() {
-    this.overlay.className = `${
-      this.state.zoomed ? "zoomer__backdrop" : "zoomer__backdrop--hidden"
-    }`;
-    document.querySelector("body").classList.toggle("zoomer__no-overflow");
-
-    if (this.state.zoomed) {
-      document
-        .querySelector("body")
-        .addEventListener("keydown", this._keyPressListener);
-      document.querySelector("body").addEventListener("wheel", this._zoomOut);
-      window.addEventListener("resize", this._zoomOut);
-    }
-  }
-
-  render() {
-    const { imgSrc, imgAlt, imgTitle, zoomId } = this.props;
-
-    return (
-      <div className="zoomer" id={`zoomer-${zoomId}`}>
-        <div
+  return (
+    <div className="zoomer" id={`zoomer-${zoomId}`}>
+      <div
+        style={{
+          zIndex: state.transitioning ? 200 : null,
+          pointerEvents: state.transitioning ? 'none' : 'auto',
+          transform: `translate(${state.offsetX}px, ${state.zoomed ? state.offsetY + "px" : "0"})`
+        }}
+        className={`zoomer__img ${state.zoomed ? "zoomer__img--zoomed" : ""}`}
+      >
+        <img
+          src={imgSrc}
+          alt={imgAlt ? imgAlt : null}
+          title={imgTitle ? imgTitle : null}
           style={{
-            zIndex: this.state.transitioning || this.state.zoomed ? 200 : null,
-            transform: `translate(${this.state.offsetX}px, ${
-              this.state.zoomed ? this.state.offsetY + "px" : "0"
-            })`
+            transform: state.zoomed ? `scale(${state.scale})` : null
           }}
-          className={`zoomer__img ${
-            this.state.zoomed ? "zoomer__img--zoomed" : ""
-          }`}
-        >
-          <img
-            src={imgSrc}
-            alt={imgAlt ? imgAlt : null}
-            title={imgTitle ? imgTitle : null}
-            style={{
-              transform: this.state.zoomed ? `scale(${this.state.scale})` : null
-            }}
-            onClick={this.zoom}
-          />
-        </div>
+          onClick={zoom}
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default ZoomerImage;
